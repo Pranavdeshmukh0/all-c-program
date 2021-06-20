4e35cf71707c7f599f5a4af9046960e0fbf14d23
Pranavdeshmukh0 <85177053+Pranavdeshmukh0@users.noreply.github.com>
2021-06-20 21:07:01 +0530

    Add files via upload

---
+++ 01ASCII.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int val = 0;
+    char ch = 'A';
+
+    printf("\n enter an ASCII value => ");
+    scanf("%d",&val);
+
+    ch = val;
+
+    printf("\n\n character for given ASCII values is = %c",ch);
+
+    return 0;
+}
---
+++ using do while loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 0;
+
+    do
+    {
+        printf("\n hello world");
+        i++;
+        printf("\n*******========*******");
+
+    }
+    while(i < 10);
+
+    return 0;
+}
